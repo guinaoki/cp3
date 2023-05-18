33a7f3eb116654b32674d3794716847162e6e6e8
ErikTeixeira <erik.barbosa515@gmail.com>
2023-05-18 16:24:31 -0300

    arrumando o formulario

--- js/cp3.js
+++ js/cp3.js
@@ -1,78 +1,81 @@
-function validarPrimeiroNome() {
-    const primeiroNomeInput = document.getElementById('primeiroNome');
-    const valor = primeiroNomeInput.value;
-    const minLength = parseInt(primeiroNomeInput.getAttribute('minlength'));
-
-    if (valor.length < minLength) {
-        primeiroNomeInput.style.border = '1px solid red';
-        alert('O primeiro nome deve ter no mínimo ' + minLength + ' caracteres.');
-    } else {
-        primeiroNomeInput.style.border = '';
-    }
-}
-
-function validarSegundoNome() {
-    const segundoNome = document.getElementById('segundoNome').value;
-    const erroSegundoNome = document.getElementById('erroSegundoNome');
-
-    if (segundoNome.length < 5) {
-        erroSegundoNome.innerHTML = 'O Segundo Nome deve conter no mínimo 5 caracteres.';
-    } else {
-        erroSegundoNome.innerHTML = '';
-    }
-}
-
-function validarEmail() {
-    const email = document.getElementById('email').value;
-    const erroEmail = document.getElementById('erroEmail');
-
-    if (email.length < 5) {
-        erroEmail.innerHTML = 'O Email deve conter no mínimo 5 caracteres.';
-    } else if (!email.includes('@')) {
-        erroEmail.innerHTML = 'O Email deve conter o caractere "@"';
-    } else {
-        erroEmail.innerHTML = '';
-    }
-}
-
-function validarSenha() {
-    const senha = document.getElementById('senha').value;
-    const confirmacaoSenha = document.getElementById('confirmacaoSenha').value;
-    const erroSenha = document.getElementById('erroSenha');
-
-    if (senha.length < 6 || senha.length > 8) {
-        erroSenha.innerHTML = 'A Senha deve conter entre 6 e 8 caracteres.';
-    } else if (senha !== confirmacaoSenha) {
-        erroSenha.innerHTML = 'A Confirmação de Senha não corresponde à senha digitada.';
-    } else {
-        erroSenha.innerHTML = '';
-    }
-}
-
-function validarConfirmacaoSenha() {
-    const senhaInput = document.getElementById('senha');
-    const confirmacaoSenhaInput = document.getElementById('confirmacaoSenha');
-    const senha = senhaInput.value;
-    const confirmacaoSenha = confirmacaoSenhaInput.value;
-
-    if (senha !== confirmacaoSenha) {
-        confirmacaoSenhaInput.style.border = '1px solid red';
-        alert('A confirmação de senha não corresponde à senha informada.');
-    } else {
-        confirmacaoSenhaInput.style.border = '';
-    }
-}
-
-function validarFormulario() {
+document.getElementById('meuFormulario').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+  
     validarPrimeiroNome();
     validarSegundoNome();
     validarEmail();
     validarSenha();
     validarConfirmacaoSenha();
-
+  
     // Se houver algum campo inválido, retorna false para impedir o envio do formulário
-    return !document.querySelectorAll('.invalid').length;
-}
+    if (document.querySelectorAll('.erro').length > 0) {
+      return false;
+    }
+  
+    // Aqui você pode prosseguir com o envio do formulário ou realizar outras ações desejadas.
+    // Por exemplo, você pode enviar os dados para um servidor usando AJAX ou realizar outras manipulações.
+  
+    alert('Formulário enviado com sucesso!');
+    // Aqui você pode fazer outras ações, como limpar os campos do formulário ou redirecionar para outra página.
+  });
+  
+  function validarPrimeiroNome() {
+    const primeiroNomeInput = document.getElementById('primeiroNome');
+    const valor = primeiroNomeInput.value;
+  
+    if (valor.length < 5) {
+      document.getElementById('erroPrimeiroNome').innerHTML = 'O primeiro nome deve ter no mínimo 5 caracteres.';
+    } else {
+      document.getElementById('erroPrimeiroNome').innerHTML = '';
+    }
+  }
+  
+  function validarSegundoNome() {
+    const segundoNomeInput = document.getElementById('segundoNome');
+    const valor = segundoNomeInput.value;
+  
+    if (valor.length < 5) {
+      document.getElementById('erroSegundoNome').innerHTML = 'O segundo nome deve ter no mínimo 5 caracteres.';
+    } else {
+      document.getElementById('erroSegundoNome').innerHTML = '';
+    }
+  }
+  
+  function validarEmail() {
+    const emailInput = document.getElementById('email');
+    const valor = emailInput.value;
+  
+    if (valor.length < 5 || !valor.includes('@')) {
+      document.getElementById('erroEmail').innerHTML = 'O email deve ter no mínimo 5 caracteres e conter o caractere "@".';
+    } else {
+      document.getElementById('erroEmail').innerHTML = '';
+    }
+  }
+  
+  function validarSenha() {
+    const senhaInput = document.getElementById('senha');
+    const valor = senhaInput.value;
+  
+    if (valor.length < 6 || valor.length > 8) {
+      document.getElementById('erroSenha').innerHTML = 'A senha deve ter entre 6 e 8 caracteres.';
+    } else {
+      document.getElementById('erroSenha').innerHTML = '';
+    }
+  }
+  
+  function validarConfirmacaoSenha() {
+    const senhaInput = document.getElementById('senha');
+    const confirmacaoSenhaInput = document.getElementById('confirmacaoSenha');
+    const senha = senhaInput.value;
+    const confirmacaoSenha = confirmacaoSenhaInput.value;
+  
+    if (senha !== confirmacaoSenha) {
+      document.getElementById('erroConfirmacaoSenha').innerHTML = 'A confirmação de senha não corresponde à senha informada.';
+    } else {
+      document.getElementById('erroConfirmacaoSenha').innerHTML = '';
+    }
+  }
+
 
 
 function alternarDarkMode() {
