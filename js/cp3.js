187d8ecde0f925cee634ec84584260fccf7b7bb9
ErikTeixeira <erik.barbosa515@gmail.com>
2023-05-18 15:59:10 -0300

    arrumando o formulario do js

--- js/cp3.js
+++ js/cp3.js
@@ -12,18 +12,41 @@ function validarPrimeiroNome() {
 }
 
 function validarSegundoNome() {
-    // Implemente a função de validação para o campo Segundo Nome
-    // seguindo o mesmo padrão da função validarPrimeiroNome
+    const segundoNome = document.getElementById('segundoNome').value;
+    const erroSegundoNome = document.getElementById('erroSegundoNome');
+
+    if (segundoNome.length < 5) {
+        erroSegundoNome.innerHTML = 'O Segundo Nome deve conter no mínimo 5 caracteres.';
+    } else {
+        erroSegundoNome.innerHTML = '';
+    }
 }
 
 function validarEmail() {
-    // Implemente a função de validação para o campo Email
-    // seguindo o mesmo padrão da função validarPrimeiroNome
+    const email = document.getElementById('email').value;
+    const erroEmail = document.getElementById('erroEmail');
+
+    if (email.length < 5) {
+        erroEmail.innerHTML = 'O Email deve conter no mínimo 5 caracteres.';
+    } else if (!email.includes('@')) {
+        erroEmail.innerHTML = 'O Email deve conter o caractere "@"';
+    } else {
+        erroEmail.innerHTML = '';
+    }
 }
 
 function validarSenha() {
-    // Implemente a função de validação para o campo Senha
-    // seguindo o mesmo padrão da função validarPrimeiroNome
+    const senha = document.getElementById('senha').value;
+    const confirmacaoSenha = document.getElementById('confirmacaoSenha').value;
+    const erroSenha = document.getElementById('erroSenha');
+
+    if (senha.length < 6 || senha.length > 8) {
+        erroSenha.innerHTML = 'A Senha deve conter entre 6 e 8 caracteres.';
+    } else if (senha !== confirmacaoSenha) {
+        erroSenha.innerHTML = 'A Confirmação de Senha não corresponde à senha digitada.';
+    } else {
+        erroSenha.innerHTML = '';
+    }
 }
 
 function validarConfirmacaoSenha() {
